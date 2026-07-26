2f4e0b022b6b86c643f1f52dbdd50b2ad63ab69f
prastyoguntur982-hub <prastyoguntur982@gmail.com>
2026-07-26 11:15:41 +0700

    feat:componets reusable untuk fungsi login

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import ProtectedRoute from '../components/layout/ProtectedRoute';
 import MainLayout from '../components/layout/MainLayout';
+
+// Auth Page
+import LoginPage from '../pages/auth/LoginPage'; 
+
+// Main Pages
 import DashboardPage from '../pages/dashboard/DashboardPage';
 import RumahListPage from '../pages/rumah/RumahListPage';
 import PenghuniListPage from '../pages/penghuni/PenghuniListPage';
-
 import PemasukanIuranPage from '../pages/transaksi/pemasukan/PemasukanIuranPage';
 import PengeluaranRtPage from '../pages/transaksi/pengeluaran/PengeluaranRtPage';
 import LaporanFinansialPage from '../pages/laporan/LaporanFinansialPage'; 
@@ -12,24 +17,31 @@ export default function AppRouter() {
   return (
     <BrowserRouter> 
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<DashboardPage />} />
-          <Route path="rumah" element={<RumahListPage />} />
-          <Route path="penghuni" element={<PenghuniListPage />} />
-          
-          {/* Path Transaksi */}
-          <Route path="transaksi">
-            <Route path="pemasukan" element={<PemasukanIuranPage />} />
-            <Route path="pengeluaran" element={<PengeluaranRtPage />} />
-          </Route>
+        {/* Public Route (Login) */}
+        <Route path="/login" element={<LoginPage />} />
 
-          {/* Path Laporan */}
-          <Route path="laporan">
-            <Route path="finansial" element={<LaporanFinansialPage />} />
-          </Route>
+        {/* Protected Routes (Harus Login) */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<DashboardPage />} />
+            <Route path="rumah" element={<RumahListPage />} />
+            <Route path="penghuni" element={<PenghuniListPage />} />
+            
+            {/* Path Transaksi */}
+            <Route path="transaksi">
+              <Route path="pemasukan" element={<PemasukanIuranPage />} />
+              <Route path="pengeluaran" element={<PengeluaranRtPage />} />
+            </Route>
 
-          <Route path="*" element={<Navigate to="/" replace />} />
+            {/* Path Laporan */}
+            <Route path="laporan">
+              <Route path="finansial" element={<LaporanFinansialPage />} />
+            </Route>
+          </Route>
         </Route>
+
+        {/* Fallback Route: Jika route tidak ditemukan */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
